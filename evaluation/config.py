edec96345636711402e7d40ce17ebade43707b3a
Ricardo Sosa Melo <r.sosamelo@gmail.com>
2023-06-20 23:38:26 +0200

    Deleted all evaluation files. They are now in server-task-manager

--- evaluation/config.py
+++
@@ -1,9 +0,0 @@
-from dotenv import dotenv_values
-import os
-
-
-config = {
-    **dotenv_values(".env"),
-    **os.environ,
-    "token_valid_time": 60 * 60 * 24 * 2 # in [s]  -  2 Days  
-}
--- evaluation/constants.py
+++
@@ -1,5 +0,0 @@
-CHECKPOINTS_PATH = "/data/nn-checkpoints"
-CONFIGS_PATH = "/data/nn-configs"
-CATEGORIES_PATH = "/data/nn-categories"
-IMAGES_PATH = "/data/images"
-OUTPUT_PATH = "/data/output"
