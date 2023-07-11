6b7a88b1f0161d87475fd47ff9482e0c12875675
Ricardo Sosa Melo <r.sosamelo@gmail.com>
2023-07-11 17:47:15 +0200

    Debug

--- configs/_user_/yolox_s.py
+++ configs/_user_/yolox_s.py
@@ -1,10 +1,27 @@
 _base_ = ['../_base_/schedules/schedule_1x.py', '../_base_/default_runtime.py']
 
-
 # Change the number of classes
+# model = dict(
+#     bbox_head=dict(num_classes=1)
+# )
+
 model = dict(
-    bbox_head=dict(num_classes=1)
-)
+    type='YOLOX',
+    input_size=(640, 360),
+    random_size_range=(15, 25),
+    random_size_interval=10,
+    backbone=dict(type='CSPDarknet', deepen_factor=0.33, widen_factor=0.5),
+    neck=dict(
+        type='YOLOXPAFPN',
+        in_channels=[128, 256, 512],
+        out_channels=128,
+        num_csp_blocks=1),
+    bbox_head=dict(
+        type='YOLOXHead', num_classes=1, in_channels=128, feat_channels=128),
+    train_cfg=dict(assigner=dict(type='SimOTAAssigner', center_radius=2.5)),
+    # In order to align the source code, the threshold of the val phase is
+    # 0.01, and the threshold of the test phase is 0.001.
+    test_cfg=dict(score_thr=0.01, nms=dict(type='nms', iou_threshold=0.65)))
 
 # Modify dataset related settings
 dataset_type = 'COCODataset'
@@ -19,7 +36,6 @@ metainfo = {
 
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
-
 
 train_pipeline = [
     dict(type='LoadImageFromFile'),
